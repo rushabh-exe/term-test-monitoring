00334fc640a75d223ed6aa0a5764485d9cca4948
rushabh-exe <rushabhmevada63@gmail.com>
2024-08-13 01:08:28 +0530

    record

--- cmd/main.go
+++ cmd/main.go
@@ -35,47 +35,47 @@ func main() {
 		AllowCredentials: true,
 	}))
 
-	adminGroup := r.Group("/admin")
+	adminGroup := r.Group("/admin") //
 	{
-		adminGroup.GET("/", admin.BaseGET)
+		adminGroup.GET("/", admin.BaseGET) //
 		adminGroup.GET("/:year/:subject/:class", students.DashboardAttendence)
 		adminGroup.DELETE("/:year/:subject/:class", students.DeleteAttendence)
 		adminGroup.PUT("/:year/:subject/:class", students.EditAttendence)
-		create := adminGroup.Group("/create")
+		create := adminGroup.Group("/create") //
 		{
-			create.POST("/timetable/:year/:sem", admin.CreateTimeTable)
-			student := create.Group("/student")
+			create.POST("/timetable/:year/:sem", admin.CreateTimeTable) //
+			student := create.Group("/student")                         //
 			{
-				student.POST("/dualAllocation", students.DualAllocation)
-				student.POST("/singleAllocation", students.SingleAllocation)
+				student.POST("/dualAllocation", students.DualAllocation)     //
+				student.POST("/singleAllocation", students.SingleAllocation) //
 			}
 			teacher := create.Group("/teacher")
 			{
-				teacher.POST("/allocation", teachers.CreateTeacherAllocation)
+				teacher.POST("/allocation", teachers.CreateTeacherAllocation) //
 				teacher.POST("/papers/:reqID/:req", teachers.MakePaperRequests)
 			}
-			vital := create.Group("/vitals")
+			vital := create.Group("/vitals") //
 			{
-				vital.GET("/", vitals.Base)
-				vital.GET("/:year", vitals.GetSubject)
-				vital.POST("/:year", vitals.CreateSubject)
-				vital.DELETE("/:year/:subject", vitals.DeleteSubject)
-				vital.GET("/teachers/:type", vitals.GetTeachers)
-				vital.POST("/createTeacher", vitals.CreateTeacher)
-				vital.PUT("/teachers/:type", vitals.EditTeacher)
-				vital.DELETE("/teachers/:type/:email", vitals.DeleteTeacher)
+				vital.GET("/", vitals.Base)                                  //
+				vital.GET("/:year", vitals.GetSubject)                       //
+				vital.POST("/:year", vitals.CreateSubject)                   //
+				vital.DELETE("/:year/:subject", vitals.DeleteSubject)        //
+				vital.GET("/teachers/:type", vitals.GetTeachers)             //
+				vital.POST("/createTeacher", vitals.CreateTeacher)           //
+				vital.PUT("/teachers/:type", vitals.EditTeacher)             //
+				vital.DELETE("/teachers/:type/:email", vitals.DeleteTeacher) //
 			}
 		}
 		get := adminGroup.Group("/get")
 		{
-			get.GET("/timetable", admin.GetTT)
-			get.GET("/timetable/:Year", admin.GetTTbyYear)
-			get.DELETE("/timetable/:year", admin.DeleteTimeTable)
-			get.GET("/student/allocation", students.GetAllocation)
-			get.DELETE("/student/allocation/:id", students.DeleteAllocation)
-			get.GET("/teacher/allocation", teachers.GetTeacherAllocation)
-			get.DELETE("/teacher/allocation/:id", teachers.DeleteTeacherAllocation)
-			get.GET("/teacher/papers", teachers.GetPaperRequests)
+			get.GET("/timetable", admin.GetTT)                                      //
+			get.GET("/timetable/:Year", admin.GetTTbyYear)                          //
+			get.DELETE("/timetable/:year", admin.DeleteTimeTable)                   //
+			get.GET("/student/allocation", students.GetAllocation)                  //
+			get.DELETE("/student/allocation/:id", students.DeleteAllocation)        //
+			get.GET("/teacher/allocation", teachers.GetTeacherAllocation)           //
+			get.DELETE("/teacher/allocation/:id", teachers.DeleteTeacherAllocation) //
+			get.GET("/teacher/papers", teachers.GetPaperRequests)                   //bad requests issue
 		}
 	}
 
@@ -83,27 +83,27 @@ func main() {
 	teacherGroup.POST("/login", auth.IsTeacherAuth)
 	teacherGroup.Use(middleware.TeacherAuthMiddleware())
 	{
-		teacherGroup.GET("/", teacher.BaseGET)
-		teacherGroup.GET("/getAttendence", attendence.Test3)
-		teacherGroup.POST("/getAttendence", attendence.CreateAttendence)
-		teacherGroup.PUT("/getAttendence", attendence.EditAttendance)
-		teacherGroup.GET("/papers", papers.GetPaperRequest)
-		teacherGroup.POST("/papers", papers.CreatePaperRequest)
-		teacherGroup.DELETE("/papers/:reqID", papers.DeletePaperRequest)
-		teacherGroup.GET("/dqc/reviews", dqcT.GetReviewRequest)
-		teacherGroup.POST("/dqc/reviews", dqcT.CreateDQCReview)
-		teacherGroup.DELETE("/dqc/reviews/:reqID", dqcT.DeleteDQCReview)
+		teacherGroup.GET("/", teacher.BaseGET)                           //
+		teacherGroup.GET("/getAttendence", attendence.Test3)             //
+		teacherGroup.POST("/getAttendence", attendence.CreateAttendence) //
+		teacherGroup.PUT("/getAttendence", attendence.EditAttendance)    //
+		teacherGroup.GET("/papers", papers.GetPaperRequest)              //
+		teacherGroup.POST("/papers", papers.CreatePaperRequest)          //
+		teacherGroup.DELETE("/papers/:reqID", papers.DeletePaperRequest) //
+		teacherGroup.GET("/dqc/reviews", dqcT.GetReviewRequest)          //
+		teacherGroup.POST("/dqc/reviews", dqcT.CreateDQCReview)          //
+		teacherGroup.DELETE("/dqc/reviews/:reqID", dqcT.DeleteDQCReview) //
 	}
 
-	api := r.Group("/api")
+	api := r.Group("/api") //not doing anything
 	{
-		api.GET("/teachers", alloc_helper.GetTeachers)
+		api.GET("/teachers", alloc_helper.GetTeachers) //not doing anything
 		// api.GET("/classroom", alloc_helper.GetClass)
 	}
 
 	dqcGroup := r.Group("/dqc")
 	dqcGroup.POST("/login", auth.IsDqcAuth)
-	dqcGroup.Use(middleware.DQCAuthMiddleware())
+	dqcGroup.Use(middleware.DQCAuthMiddleware()) //something isssue
 	{
 		dqcGroup.GET("/", func(c *gin.Context) { c.String(200, "You are at DQC routes") })
 		dqcGroup.GET("/requests", dqc.GetReviews)
